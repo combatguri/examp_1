c909d3f65c3a220621ef086c059d76b2f8008741
combatguri <combatguri92@gmail.com>
2021-09-26 15:16:58 +0900

    1 file modified

--- index.js
+++ index.js
@@ -6,29 +6,38 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 const GArray = function (argNum) {
-  const _gArray = this;
+  const _G = Object.assign(this, {
+    dataStore: [],
+    pos: argNum,
+    argNum: argNum,
+  });
 
-  _gArray.datas = [];
-
-  _gArray.init = () => {
-    /*
-    for (let i = 0; i < argNum; ++i) {
-      // const randomNum = Math.floor(Math.random() + (_gArray.numElements + 1));
-      // console.log(randomNum);
-      // _gArray.datas.push(randomNum);
-      _gArray.datas.push(i);
+  _G.setData = () => {
+    for (let i = 0; i < argNum; i++) {
+      const randomNum = Math.floor(Math.random() * argNum + 1);
+      _G.dataStore.push(randomNum);
     }
-    */
   };
 
-  _gArray.toString = () => {
-    console.log(this.datas);
+  _G.clearData = () => {
+    this.dataStore = [];
   };
 
-  _gArray.init();
+  _G.toString = () => {
+    console.log(this.dataStore);
+  };
+
+  _G.init = () => {
+    for (let i = 1; i < argNum; i++) {
+      _G.dataStore.push(i);
+    }
+  };
+
+  _G.init();
 };
 
 const numElements = 100;
-const myNumbs = new GArray(numElements);
 
-myNumbs.toString();
+// 기본정렬
+const basicSort = new GArray(numElements);
+basicSort.toString();
